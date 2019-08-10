d9251a10627d6cb2523bab1db808d0a770a04fe8
Eyal Rubowitz <eyalr26@gmail.com>
2019-08-11 01:44:36 +0300

    extend user data & css

--- src/users-components/UserListComp.js
+++ src/users-components/UserListComp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import UserTemplateComp from './UserTemplateComp';
 import userModel from '../DAL/userModel';
+import './UserListCss.css';
 
 class UserListComp extends Component {
     constructor(props) {
@@ -13,32 +14,40 @@ class UserListComp extends Component {
     }
 
     onUpdateList = () => {
-        userModel.getUsers().then( res => 
-            this.setState({userList: res})
+        userModel.getUsers().then(res =>
+            this.setState({ userList: res })
         );
     }
 
     onRefreshList = () => {
-        userModel.refreshList().then( res => 
-            this.setState({userList: res})
+        userModel.refreshList().then(res =>
+            this.setState({ userList: res })
         );
     }
 
     onHandleSearch = (e) => {
         let searchText = e.target.value;
         let searchedList = userModel.searchUserMatchList(searchText);
-        this.setState({userList: searchedList});
+        this.setState({ userList: searchedList });
     }
 
     render() {
         let users = this.state.userList.map(u => {
-            return (<UserTemplateComp key={u.id} userData={u} updateList={this.onUpdateList}/> )
+            return (<UserTemplateComp key={u.id} userData={u} updateList={this.onUpdateList} />)
         })
         return (
-            <div>
-                Search: <input type="text" onChange={this.onHandleSearch} />  
-                <input type="button" value="Refresh User List" onClick={this.onRefreshList} />
-                {users}
+            <div id='mainUserList'>
+                <div id="searchTextBox">
+                    Search: <input type="text" onChange={this.onHandleSearch} />
+                </div>
+                <div id="refreshListBtn">
+                    <input type="button" 
+                            value="Refresh User List" 
+                            onClick={this.onRefreshList} />
+                </div>
+                <div>
+                    {users}
+                </div>
             </div>
         );
     }
--- src/users-components/UserTemplateComp.js
+++ src/users-components/UserTemplateComp.js
@@ -5,21 +5,21 @@ import userModel from '../DAL/userModel';
 class UserTemplateComp extends Component {
     constructor(props) {
         super(props);
-        this.state = {userData: {}};
+        this.state = { userData: {}, isExtendUserData: false };
     }
 
     componentDidMount() {
-        this.setState({userData: this.props.userData});
+        this.setState({ userData: this.props.userData });
     }
 
     onNameChangeHandle = (e) => {
         let newName = e.target.value;
-        this.setState({userData: {...this.state.userData, name: newName}});
+        this.setState({ userData: { ...this.state.userData, name: newName } });
     }
 
     onEmailChangeHandle = (e) => {
         let newEmail = e.target.value;
-        this.setState({userData: {...this.state.userData, email: newEmail}});
+        this.setState({ userData: { ...this.state.userData, email: newEmail } });
     }
 
     onDelete = () => {
@@ -32,24 +32,49 @@ class UserTemplateComp extends Component {
         this.props.updateList();
     }
 
+    onToggleHover = (e) => {
+        console.log(e.type);
+        let bool = (e.type === 'mouseenter') ? true : false;
+        this.setState({ isExtendUserData: bool });
+    }
+
     render() {
+
+        let extendClass = (this.state.isExtendUserData) ? 'markedBtn' : '';
+
+        let extendData = undefined;
+        if (this.state.isExtendUserData === true) {
+            extendData = <div id='extendUserData'>
+                <h4>Street: &nbsp; {this.state.userData.address.street}</h4>
+                <h4>City: &nbsp; {this.state.userData.address.city}</h4>
+                <h4>Zipcode: &nbsp; {this.state.userData.address.zipcode}</h4>
+            </div>
+        }
+
         return (
-            <div>
-                <h1>Hello :)</h1>
-                ID: {this.props.userData.id}
+            <div id='mainUserDiv'>
+                <span id='userId'>ID: {this.props.userData.id}</span>
                 <br />
-                Name: <input type="text" 
-                            onChange={this.onNameChangeHandle}
-                            value={this.state.userData.name || ''}/>
+                Name: <input type="text"
+                    onChange={this.onNameChangeHandle}
+                    value={this.state.userData.name || ''} />
                 <br />
-                Email: <input type="text" 
-                              onChange={this.onEmailChangeHandle}
-                              value={this.state.userData.email || ''}/>
+                Email: <input type="text"
+                    onChange={this.onEmailChangeHandle}
+                    value={this.state.userData.email || ''} />
                 <br />
-                <input type="button" value="Other Data"/>
-                &nbsp;  &nbsp;  &nbsp;
-                <input type="button" value="Update" onClick={this.onUpdate}/>
-                <input type="button" value="Delete" onClick={this.onDelete}/>
+                <div>
+                    <input type="button"
+                        className={extendClass}
+                        value="Other Data"
+                        onMouseEnter={this.onToggleHover}
+                        onClick={this.onToggleHover} />
+                    <input type="button" value="Update" onClick={this.onUpdate} class='userChangedBtn' />
+                    <input type="button" value="Delete" onClick={this.onDelete} class='userChangedBtn' />
+                </div>
+                <div>
+                    {extendData}
+                </div>
             </div>
         );
     }
